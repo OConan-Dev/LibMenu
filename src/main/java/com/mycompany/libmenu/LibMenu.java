d8a998d326753eea925cf662fcb6cf5077836b60
OConan-Dev <conan.debijl@gmail.com>
2025-08-04 14:23:54 +0800

    Init Comit
    Creation

--- src/main/java/com/mycompany/libmenu/LibMenu.java
+++ src/main/java/com/mycompany/libmenu/LibMenu.java
@@ -11,6 +11,8 @@ package com.mycompany.libmenu;
 public class LibMenu {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+       
     }
 }
